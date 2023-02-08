f956e60b07ddbdb97385174262db2c2e260f5d74
赵龙生 <zhaolongsheng@tenwit.com>
2023-02-08 18:36:03 +0800

    fix: work around unbuild stub

--- build.config.ts
+++ build.config.ts
@@ -1,10 +1,13 @@
+import fs from 'fs';
+import path from 'path';
 import { defineBuildConfig } from 'unbuild';
+import prettier from 'prettier/standalone';
+import parserBabel from 'prettier/parser-babel';
 
 export default defineBuildConfig({
   entries: [
     {
       input: './src/node/cli',
-      name: 'cli',
     },
   ],
   clean: true,
@@ -13,5 +16,41 @@ export default defineBuildConfig({
   rollup: {
     emitCJS: true,
     cjsBridge: true,
+  },
+  hooks: {
+    'rollup:done'() {
+      if (process.env.DEV) {
+        const extensions = ['.cjs', '.mjs'];
+        for (const ext of extensions) {
+          const mjsPath = path.resolve(__dirname, 'dist', `cli${ext}`);
+          let content = fs.readFileSync(mjsPath, 'utf-8');
+          const JTIT_RE = /(interopDefault: true, esmResolve: true)/;
+          const matches = content.match(JTIT_RE);
+          if (matches && matches[0] && matches[1]) {
+            if (ext === 'mjs') {
+              content = `import dynamicImport from '@babel/plugin-proposal-dynamic-import';\n${content}`;
+            }
+            const replaceStr =
+              ext === 'mjs'
+                ? 'transformOptions: { babel: { plugins: [dynamicImport] }}'
+                : "transformOptions: {babel: { plugins: [require('@babel/plugin-proposal-dynamic-import')] }}";
+            content = content.replace(
+              matches[1],
+              `${matches[1]}, ${replaceStr},`
+            );
+            content = prettier.format(content, {
+              parser: 'babel',
+              plugins: [parserBabel],
+              tabWidth: 2,
+              useTabs: false,
+              semi: true,
+              singleQuote: true,
+              endOfLine: 'auto',
+            });
+            fs.writeFileSync(mjsPath, content);
+          }
+        }
+      }
+    },
   },
 });
