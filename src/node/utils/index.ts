4d0d59f5f1dce5b1cef1a5e3c293305ac015d7ab
赵龙生 <zhaolongsheng@tenwit.com>
2023-02-23 11:02:30 +0800

    chore: PACKAGE_ROOT

--- src/node/utils/index.ts
+++ src/node/utils/index.ts
@@ -1,3 +1,27 @@
+import path from 'path';
+import fs from 'fs';
+
 export function slash(id: string) {
   return id.replace(/\\/g, '/');
+}
+
+export function lookupFile(
+  dir: string,
+  formats: string[],
+  options?: { pathOnly: boolean }
+): string | undefined {
+  for (const format of formats) {
+    const fullPath = path.join(dir, format);
+    if (fs.existsSync(fullPath) && fs.statSync(fullPath).isFile()) {
+      const result = options?.pathOnly
+        ? fullPath
+        : fs.readFileSync(fullPath, 'utf-8');
+      return result;
+    }
+  }
+
+  const parentDir = path.dirname(dir);
+  if (parentDir !== dir) {
+    return lookupFile(parentDir, formats, options);
+  }
 }
